05af36721f07bf8fe245e018e4717cdeeb60fc47
zeeshanali009 <zeeshanali.shams@gmail.com>
2017-09-21 15:38:23 +0530

    Adding interface Function

---
+++ src/main/java/com/ali/zeeshan/week01/iface/Function.java
@@ -0,0 +1,14 @@
+package com.ali.zeeshan.week01.iface;
+/* The interface has been created with the name Function.
+ * The interface has three methods(unary, binary and ternary).
+ */
+public interface Function 
+{
+	//unary
+	public int compute(int a);
+	//binary
+	public int compute(int a,int b);
+	//ternary
+	public int compute(int a,int b,int c);
+
+}
