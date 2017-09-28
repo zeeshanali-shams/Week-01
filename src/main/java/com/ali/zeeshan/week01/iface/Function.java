c050b7230cceb67d919fee14c73afdd65ee8a9c8
zeeshanali009 <zeeshanali.shams@gmail.com>
2017-09-28 15:01:27 +0530

    Update Function.java

--- src/main/java/com/ali/zeeshan/week01/iface/Function.java
+++ src/main/java/com/ali/zeeshan/week01/iface/Function.java
@@ -1,42 +1,25 @@
 package com.ali.zeeshan.week01.iface;
-<<<<<<< HEAD
 
 /**
  * The interface has been created with the name Function. The interface has
  * three methods(unary, binary and ternary).
  */
 public interface Function {
-=======
-/** The interface has been created with the name Function.
- * The interface has three methods(unary, binary and ternary).
- */
-public interface Function 
-{
->>>>>>> 1a7965f8eee849cc755293a30ef2f564c654b310
+
 	/**
 	 * This is the unary method
 	 */
 	public int compute(int a);
-<<<<<<< HEAD
 
 	/**
-	 * This is the unary method
+	 * This is the binary method
 	 */
 	public int compute(int a, int b);
 
 	/**
-	 * This is the unary method
+	 * This is the ternary method
 	 */
 	public int compute(int a, int b, int c);
-=======
-	/**
-	 * This is the unary method
-	 */
-	public int compute(int a,int b);
-	/**
-	 * This is the unary method
-	 */
-	public int compute(int a,int b,int c);
->>>>>>> 1a7965f8eee849cc755293a30ef2f564c654b310
+
 
 }
