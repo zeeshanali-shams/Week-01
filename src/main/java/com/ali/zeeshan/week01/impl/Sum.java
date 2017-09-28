ee3dae38d953643ab27357951b83ee01bd0f4d48
zeeshanali009 <zeeshanali.shams@gmail.com>
2017-09-28 14:49:45 +0530

    Adding interface Function

--- src/main/java/com/ali/zeeshan/week01/impl/Sum.java
+++ src/main/java/com/ali/zeeshan/week01/impl/Sum.java
@@ -2,23 +2,15 @@ package com.ali.zeeshan.week01.impl;
 
 import com.ali.zeeshan.week01.iface.Function;
 
-<<<<<<< HEAD
 /**
  * This class will work as a method to perform addition.
-=======
-/**This class will work as a method to perform addition.
->>>>>>> 1a7965f8eee849cc755293a30ef2f564c654b310
  */
 public class Sum implements Function {
 	/**
 	 * This is the unary method
 	 */
 	public int compute(int a) {
-<<<<<<< HEAD
 		return a;
-=======
-		return a + 10;
->>>>>>> 1a7965f8eee849cc755293a30ef2f564c654b310
 	}
 
 	/**
@@ -32,11 +24,7 @@ public class Sum implements Function {
 	 * This is the ternary method
 	 */
 	public int compute(int a, int b, int c) {
-<<<<<<< HEAD
 		return compute(a, b) + c;
-=======
-		return a + b + c;
->>>>>>> 1a7965f8eee849cc755293a30ef2f564c654b310
 	}
 
 	public static void main(String[] args) {
